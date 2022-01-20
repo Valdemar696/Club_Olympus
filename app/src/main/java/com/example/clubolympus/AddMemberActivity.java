c26500e0bd177e2c3b330b010e0d878516665e89
Valdemar696 <valdemar.tigrov@gmail.com>
2022-01-20 11:20:32 +0300

    Added addMemberActivity

--- app/src/main/java/com/example/clubolympus/AddMemberActivity.java
+++ app/src/main/java/com/example/clubolympus/AddMemberActivity.java
@@ -1,11 +1,20 @@
 package com.example.clubolympus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class AddMemberActivity extends AppCompatActivity {
 
@@ -14,6 +23,8 @@ public class AddMemberActivity extends AppCompatActivity {
     private Spinner genderSpinner;
     private EditText groupEditText;
     private int gender = 0;
+
+    private ArrayAdapter spinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,50 @@ public class AddMemberActivity extends AppCompatActivity {
         genderSpinner = findViewById(R.id.genderSpinner);
         groupEditText = findViewById(R.id.groupEditText);
 
-        ArrayAdapter
+        spinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.array_gender, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(spinnerAdapter);
+
+        genderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedGender = (String)adapterView.getItemAtPosition(i);
+                if (!TextUtils.isEmpty(selectedGender)) {
+                    if (selectedGender.equals("Male")) {
+                        gender = 1;
+                    } else if (selectedGender.equals("Female")) {
+                        gender = 2;
+                    } else {
+                        gender = 0;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                gender = 0;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_member_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_member:
+                return true;
+            case R.id.delete_member:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
